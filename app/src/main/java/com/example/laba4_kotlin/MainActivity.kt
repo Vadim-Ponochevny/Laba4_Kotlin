caf8e7326dbc027d4288690c0a9b01bd5847d504
Vadim-Ponochevny <Ponovadim67@mail.ru>
2025-04-15 14:36:18 +0700

    Task-3 (InternetTest)

--- app/src/main/java/com/example/laba4_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/laba4_kotlin/MainActivity.kt
@@ -1,16 +1,40 @@
 package com.example.laba4_kotlin
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val tag = "Flickr cats"
+        val internetButton: Button = findViewById(R.id.btnHTTP)
+
+        internetButton.setOnClickListener{
+            val url = URL("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ff49fcd4d4a08aa6aafb6ea3de826464&tags=cat&format=json&nojsoncallback=1")
+
+            Thread {
+                val connection1 = url.openConnection() as HttpURLConnection
+                try {
+                    val data = connection1.inputStream.bufferedReader().readText()
+                    connection1.disconnect()
+                    Log.d(tag, data)
+                }
+                catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }.start()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
